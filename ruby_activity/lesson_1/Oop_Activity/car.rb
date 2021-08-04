b5e89b0baf0ee0b931dd8b21a1956d9c5868f775
Carl See <carlsee06@gmail.com>
2021-08-04 20:12:53 +0800

    object activity

---
+++ ruby_activity/lesson_1/Oop_Activity/car.rb
@@ -0,0 +1,14 @@
+class Car
+    def initialize(name, color, type)
+      @name = name
+      @color = color
+      @type = type
+    end
+  
+    def start
+      puts "I'm #{@name} and here is a #{@color} #{@type} car!"
+    end
+  end
+  
+  toyota = Car.new("mikey", "yellow", "toyota suv")
+  toyota.start
